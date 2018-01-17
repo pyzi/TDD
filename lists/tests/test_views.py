be81afbb9212904f76563f75e6f6f738d6e05307
tandavala <tandavala@github.com>
2018-01-17 17:04:03 +0100

    updating the travis file

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,6 +3,8 @@ import unittest
 from django.test import TestCase
 
 class HomePagetTest(TestCase):
+    """ Testing the  home page. """
+    
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
